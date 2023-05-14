6e08c5c7fe474c44e80e4188724810cb3ef17e41
Arcturus91 <arturo.barrantesv@pucp.pe>
2023-05-14 03:31:06 -0500

    Implement Apollo server

--- server.js
+++ server.js
@@ -1,36 +1,36 @@
-const express = require("express");
 const path = require("path");
-const { graphqlHTTP } = require("express-graphql"); //express middleware function that responds to graphql queries.
+const express = require("express");
+const { ApolloServer } = require("apollo-server-express");
 const { loadFilesSync } = require("@graphql-tools/load-files");
 const { makeExecutableSchema } = require("@graphql-tools/schema");
+const { start } = require("repl");
 
 const typesArray = loadFilesSync("**/*", {
   extensions: ["graphql"],
 });
 const resolversArray = loadFilesSync(path.join(__dirname, "**/*.resolvers.js"));
 
-const schema = makeExecutableSchema({
-  typeDefs: typesArray,
-  resolvers: resolversArray, //estos son los que deben retornar un resultado. por eso cuando importamos las funciones de los models en los resolvers files, las ejecutamos
-});
+async function startApolloServer() {
+  const app = express();
+  const schema = makeExecutableSchema({
+    typeDefs: typesArray,
+    resolvers: resolversArray, //estos son los que deben retornar un resultado. por eso cuando importamos las funciones de los models en los resolvers files, las ejecutamos
+  });
+  /* This line is importing the buildSchema function from the GraphQL library, 
+  which is used to define the data schema for the GraphQL API. */
 
-/* This line is importing the buildSchema function from the GraphQL library, 
-which is used to define the data schema for the GraphQL API. */
+  const server = new ApolloServer({
+    schema,
+  });
 
-const app = express();
+  await server.start();
+  server.applyMiddleware({ app, path: "/graphql" });
 
-app.use(
-  "/graphql", //this is the path where we are mounting the middleware
-  graphqlHTTP({
-    schema: schema,
-    graphiql: true,
-  })
-);
-/* - This line mounts the graphqlHTTP middleware at the root path */
-
-app.listen(3000, () => {
-  console.log("Running GraphQL server...");
-});
+  app.listen(3000, () => {
+    console.log("Running GraphQL server...");
+  });
+}
+startApolloServer();
 
 /* const root = {
   products: require("./products/products.model"),
@@ -43,3 +43,17 @@ app.listen(3000, () => {
   rootValue: root, no longer needed since all resolver are coming from schema and maeExecutableSchema
   graphiql: true,
 }) */
+
+/* for using apollo server,
+we have to uninstall:
+const { graphqlHTTP } = require("express-graphql"); //express middleware function that responds to graphql queries.
+ */
+
+/* app.use(
+  "/graphql", //this is the path where we are mounting the middleware
+  graphqlHTTP({
+    schema: schema,
+    graphiql: true,
+  })
+); */
+/* - This line mounts the graphqlHTTP middleware at the root path */
